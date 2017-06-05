cac3f90da1205e948dc91636f2dbd3c34a1ab7e6
keerthanakavitha <keerthuharsu@gmail.com>
2017-06-05 15:19:57 +0530

    Create gre.java

---
+++ gre.java
@@ -0,0 +1,20 @@
+class program
+{
+public static void main(string[]args)
+{
+int a,b,c;
+System.out.println("enter the number:");
+if(a>b&&a>c)
+{
+System.out.println("a is greater:"+a);
+}
+else if(b>c)
+{
+Sytem.out.println("b is greater:"+b);
+}
+else
+{
+System.out.println("c is greater:"+c);
+}
+}
+}
