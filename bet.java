d52a4de1470bf286dcd09ea579b8d3430b89760f
keerthanakavitha <keerthuharsu@gmail.com>
2017-06-05 15:07:20 +0530

    Create bet.java

---
+++ bet.java
@@ -0,0 +1,12 @@
+class program
+{
+public staatic void main(String[]args)
+{
+char c;
+System.out.println("enter the character");
+if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+System.out.println("is alphabet"+c);
+else
+System.out.println("is not a alphabet"+c);
+}
+}
