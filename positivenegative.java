8fa9900d7146303ab586e7dba172e38675bdf96c
keerthanakavitha <keerthuharsu@gmail.com>
2017-06-05 12:17:58 +0530

    Create positivenegative.java

---
+++ positivenegative.java
@@ -0,0 +1,20 @@
+class program
+{
+public static void main(String[]args)
+{
+int a;
+System.out.println("enter the number:"+a);
+if(a>0)
+{
+System.out.println("positive number");
+}
+else if(a<0)
+{
+System.out.println("negative number");
+}
+else
+{
+System.out.println("zero");
+}
+}
+}
