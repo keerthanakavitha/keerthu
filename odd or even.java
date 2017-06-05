b67384c3657189191cbb4c79b9a766910152a879
keerthanakavitha <keerthuharsu@gmail.com>
2017-06-05 11:53:57 +0530

    Create odd or even.java

---
+++ odd or even.java
@@ -0,0 +1,11 @@
+class program
+{
+public static void main(string arg[])
+{
+int n=2;
+if(n%2==0)
+system.out.println("entered number is even");
+else
+system.out.println("entered number is odd");
+}
+}
