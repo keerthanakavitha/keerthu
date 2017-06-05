57bc40b89d0637ea2e885c51fb44738a446aa137
keerthanakavitha <keerthuharsu@gmail.com>
2017-06-05 14:53:06 +0530

    Create digit.java

---
+++ digit.java
@@ -0,0 +1,13 @@
+class program
+{
+public static void main(string[]args)
+{
+int i,n,sum=0;
+System.out.println("enter the number"+i);
+for(i=1;i<=n;i++)
+{
+sum+=i;
+}
+System.out.println("sum is:"+sum);
+}
+}
