6739453f194db2b3cbc8bbd60dac6897ed409677
keerthanakavitha <keerthuharsu@gmail.com>
2017-06-06 12:08:14 +0530

    Create number.java

---
+++ number.java
@@ -0,0 +1,14 @@
+class program
+{
+public static void main(String[]args)
+{
+int a,s=0;
+System.out.println("enter the value:");
+while(a>0)
+{
+a=a/10;
+a++;
+}
+System.out.println("number of digits is:"+a);
+}
+}
