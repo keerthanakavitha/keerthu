aeb13fd874d20ab5728ca32f230d7bc24812d2b0
keerthanakavitha <keerthuharsu@gmail.com>
2017-06-06 11:59:49 +0530

    Create greater.java

---
+++ greater.java
@@ -0,0 +1,20 @@
+class program
+{
+public static void main(String[]args)
+{
+int a,b and c;
+System.out.println("enter the values of a,b and c");
+if(a>b && a>c)
+{
+System.out.println("a is greater:"+a);
+}
+else if(b>c)
+{
+System.out.println("b is greater:"+b);
+}
+else
+{
+System.out.println("c is greater:"+c);
+}
+}
+}
