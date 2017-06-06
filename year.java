62aedf5b2752b71df6c2a90f4fe58dda01707c91
keerthanakavitha <keerthuharsu@gmail.com>
2017-06-06 12:18:45 +0530

    Create year.java

---
+++ year.java
@@ -0,0 +1,24 @@
+class program
+{
+public static void main(String[]args)
+{
+int year;
+System.out.println("enter the year:");
+if((year%4==0)&&(year%100!=0)
+{
+System.out.println("is a leap year:"+year);
+}
+else if(year%100==0)
+{
+System.out.println("is not a leap year:"+year);
+}
+else if(year%400==0)
+{
+System.out.printtln("is a leap year:"+year);
+}
+else
+{
+System.out.println("is not a leap year:"+year);
+}
+}
+}
