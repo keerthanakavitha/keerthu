9fb24904084379de9b291e085cb1ff2dcd592ef7
keerthanakavitha <keerthuharsu@gmail.com>
2017-06-06 12:24:34 +0530

    Create s.java

---
+++ s.java
@@ -0,0 +1,13 @@
+class program
+{
+public static void main(String[]args)
+{
+int i,n,sum=0;
+System.out.println("enter the number:");
+for(i=1;i<=n;i++)
+{
+sum+=i;
+}
+System.out.println("sum is:"+sum);
+}
+}
